79aa3967040a2490bcfdd65c7d67009e6ce178df
Steven Corzo <stevenbleb@gmail.com>
2023-09-24 17:41:28 -0400

    login stuff

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,20 @@
 import {FaSignInAlt,FaSignOutAlt,FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
+import { useSelector,useDispatch } from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice'
 
 
 function Header() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state)=> state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
   return (
     <header className = 'header'>
         <div className = 'logo'>
@@ -10,7 +22,13 @@ function Header() {
         </div>
 
         <ul>
+            {user ? (
             <li>
+                <button className = 'btn' onClick = {onLogout}>
+                <FaSignOutAlt/> Logout
+                </button>
+            </li>) 
+               : (<> <li>
                 <Link to = '/login'>
                     <FaSignInAlt/> Login
 
@@ -21,7 +39,8 @@ function Header() {
                     <FaUser/> Register
 
                 </Link>
-            </li>
+            </li></>)}
+           
         </ul>
     
     </header>
